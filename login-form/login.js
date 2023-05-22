334bd05e538dddf3f0777c7b2554a7f78533fa4b
DinhTungQN <129242546+DinhTungQN@users.noreply.github.com>
2023-05-22 08:40:01 +0700

    Add files via upload

---
+++ login-form/login.js
@@ -0,0 +1,15 @@
+var app = angular.module("myApp", []);
+
+app.controller("myCtrl", function($scope)
+{
+    $scope.showMe = false;
+    $scope.addContact = function() {
+            if ($scope.newContact.email == ''){
+                $scope.showMe = true;
+            } else {
+                $scope.newContact={};
+                $scope.showMe = false;            
+                alert('Logged in successfully')
+            }
+    }
+});
